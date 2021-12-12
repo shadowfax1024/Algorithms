58cc5441dd52177aaaa63bbd9a0ba30077b47809
shadowfax1024 <95575649+shadowfax1024@users.noreply.github.com>
2021-12-12 16:12:15 +0530

    Update count_inversions.py

--- count_inversions.py
+++ count_inversions.py
@@ -20,10 +20,7 @@ def merge(x,y): #x,y are lists can be of unequal lengths
             res.append(x[p])
             p +=1
     print(res)
-    return res #inversions #res,inversions
-#a = [1,4,7]
-#b = [2,5,9,11,29,100]
-#print(count_inversions(a,b))
+    return res
 def merge_sort(nums):  # proper implementation of merge sort algorithm
     if(len(nums)==1):
         return nums #nums,0
@@ -36,12 +33,6 @@ def merge_sort(nums):  # proper implementation of merge sort algorithm
        # left,inv_left = merge_sort(nums[:mid])
         #right,inv_right = merge_sort(nums[mid:])
         return merge(inv_left,inv_right)
-#x = [5,2,6,1]
-#x=[-1,-1]
+    
 x =[2,4,3,5,1]
-#x =[100]
-#res = [0]*len(x)
-#for i in range(len(x)):
-    #res[i] = merge_sort(x[i:])
-#print(res)    
 print(merge_sort(x))
